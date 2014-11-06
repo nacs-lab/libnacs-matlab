8adf1074a8001642f98fb93947eac07232579a99
Yichao Yu <yyc1992@gmail.com>
2014-11-06 17:33:52 -0500

    [matlab_new/funcPulse] add

---
+++ funcPulse.m
@@ -0,0 +1,27 @@
+%% Copyright (c) 2014-2014, Yichao Yu <yyc1992@gmail.com>
+%%
+%% This library is free software; you can redistribute it and/or
+%% modify it under the terms of the GNU Lesser General Public
+%% License as published by the Free Software Foundation; either
+%% version 3.0 of the License, or (at your option) any later version.
+%% This library is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+%% Lesser General Public License for more details.
+%% You should have received a copy of the GNU Lesser General Public
+%% License along with this library.
+
+classdef funcPulse < handle
+  properties(Access=private)
+    func;
+  end
+
+  methods
+    function self = funcPulse(func)
+      self.func = func;
+    end
+    function val = calcValue(self, t, len, old_val)
+      val = self.func(t, len, old_val);
+    end
+  end
+end
