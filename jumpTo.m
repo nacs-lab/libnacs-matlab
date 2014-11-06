16cdc81c154d24075de5cd523be9f29316fd6336
Yichao Yu <yyc1992@gmail.com>
2014-11-06 17:45:38 -0500

    [matlab_new/jumpTo] add

---
+++ jumpTo.m
@@ -0,0 +1,45 @@
+%% Copyright (c) 2014-2014, Yichao Yu <yyc1992@gmail.com>
+%%
+%% This library is free software; you can redistribute it and/or
+%% modify it under the terms of the GNU Lesser General Public
+%% License as published by the Free Software Foundation; either
+%% version 3.0 of the License, or (at your option) any later version.
+%% This library is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+%% Lesser General Public License for more details.
+%% You should have received a copy of the GNU Lesser General Public
+%% License along with this library.
+
+classdef jumpTo < handle
+  properties(Access=private)
+    val;
+    time;
+  end
+
+  methods
+    function self = jumpTo(v, t)
+      if nargin <= 1
+        t = 0;
+      elseif t < 0;
+        error('Cannot jump value at negative time.');
+      end
+      self.val = v;
+      self.time = t;
+    end
+    function [tstart, tlen] = timeSpan(self, len, old_val)
+      tstart = self.time;
+      tlen = 0;
+    end
+    function times = dirtyTime(self)
+      times = [self.time];
+    end
+    function val = calcValue(self, t, len, old_val)
+      if t < self.time
+        val = old_val;
+      else
+        val = self.val;
+      end
+    end
+  end
+end
