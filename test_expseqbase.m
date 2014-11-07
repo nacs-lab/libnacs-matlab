ca0f388fc27cf3e88222498cafac4691084c31b1
nigrp <niharvardlab@gmail.com>
2014-11-07 11:22:42 -0500

    [matlab_new] test

--- test_expseqbase.m
+++ test_expseqbase.m
@@ -40,38 +40,53 @@ function test_expseqbase()
   seq.addStep(1, @addstep_tester);
   seq.addStep(1, 'addstep_tester', 2);
 
-  pulses1 = tseq.getPulses('1');
-  pulses2 = tseq.getPulses('2');
-  pulses3 = tseq.getPulses('3');
+  pulses1 = seq.getPulses('1');
+  pulses2 = seq.getPulses('2');
+  pulses3 = seq.getPulses('3');
 
   assert(size(pulses1, 1) == 12);
   assert(size(pulses2, 1) == 1);
   assert(size(pulses3, 1) == 2);
 
-  assert(pulses1{1, 1} == 0);
-  assert(pulses1{1, 2} == 2);
-  assert(pulses1{1, 4} == 0);
-  assert(pulses1{1, 5} == 2);
+  pulses1_expected = {{0, 1, 0, 1}, ...
+                      {1, 4, 1, 4}, ...
+                      {6, 1, 6, 1}, ...
+                      {7, 0, 7, 1}, ...
+                      {7.5, 0, 7, 1}, ...
+                      {9, 0, 9, 1}, ...
+                      {11, 0, 11, 1}, ...
+                      {11.5, 0, 11, 1}, ...
+                      {13, 0, 13, 1}, ...
+                      {15, 0, 15, 2}, ...
+                      {16, 0, 15, 2}, ...
+                      {18, 0, 18, 2}};
 
-  assert(pulses1{2, 1} == 2);
-  assert(pulses1{2, 2} == 3);
-  assert(pulses1{2, 4} == 2);
-  assert(pulses1{2, 5} == 3);
+  for i = 1:size(pulses1, 1)
+    assert(pulses1{i, 1} == pulses1_expected{i}{1});
+    assert(pulses1{i, 2} == pulses1_expected{i}{2});
+    assert(pulses1{i, 4} == pulses1_expected{i}{3});
+    assert(pulses1{i, 5} == pulses1_expected{i}{4});
+    assert(pulses1{i, 6} == '1');
+  end
 
+  pulses2_expected = {{1, 4, 1, 4}};
 
-  assert(pulses2{1, 1} == 1);
-  assert(pulses2{1, 2} == 2);
-  assert(pulses2{1, 4} == 1);
-  assert(pulses2{1, 5} == 2);
+  for i = 1:size(pulses2, 1)
+    assert(pulses2{i, 1} == pulses2_expected{i}{1});
+    assert(pulses2{i, 2} == pulses2_expected{i}{2});
+    assert(pulses2{i, 4} == pulses2_expected{i}{3});
+    assert(pulses2{i, 5} == pulses2_expected{i}{4});
+    assert(pulses2{i, 6} == '2');
+  end
 
-  assert(pulses2{2, 1} == 6);
-  assert(pulses2{2, 2} == 0);
-  assert(pulses2{2, 4} == 6);
-  assert(pulses2{2, 5} == 2);
+  pulses3_expected = {{0, 1, 0, 1}, ...
+                      {6, 1, 6, 1}};
 
-
-  assert(pulses3{1, 1} == 0);
-  assert(pulses3{1, 2} == 2);
-  assert(pulses3{1, 4} == 0);
-  assert(pulses3{1, 5} == 2);
+  for i = 1:size(pulses3, 1)
+    assert(pulses3{i, 1} == pulses3_expected{i}{1});
+    assert(pulses3{i, 2} == pulses3_expected{i}{2});
+    assert(pulses3{i, 4} == pulses3_expected{i}{3});
+    assert(pulses3{i, 5} == pulses3_expected{i}{4});
+    assert(pulses3{i, 6} == '3');
+  end
 end
