caf82a4cec67c72446e2171cf697b90e1e925ac3
Yichao Yu <yyc1992@gmail.com>
2014-11-08 12:20:56 -0500

    [matlab_new/pulseBase] add pulse id

--- pulseBase.m
+++ pulseBase.m
@@ -12,26 +12,48 @@
 %% License along with this library.
 
 classdef(Abstract) pulseBase < handle
+  properties(Access=private)
+    id;
+  end
   methods(Abstract=true)
     %% Old value is the value of the channel at tstart returned by timeSpan.
     val = calcValue(self, t, len, old_val);
   end
 
   methods
+    function self = pulseBase()
+      %% Cannot be multithread-safe. FIXME
+      %% (when porting to a real programming language)
+      global nacsPulseBaseID;
+      if isempty(nacsPulseBaseID)
+        nacsPulseBaseID = 1;
+      else
+        nacsPulseBaseID = nacsPulseBaseID + 1;
+      end
+      self.id = nacsPulseBaseID;
+    end
+
+    function id = getID(self)
+      id = self.id;
+    end
+
     function res = hasDirtyTime(self, len)
       %% TODO? cache
       res = ~isempty(self.dirtyTime(len));
     end
+
     function avail = available(self, t, dt, len)
       [tstart, tlen] = self.timeSpan(len);
       %% The time availability check (in general) might miss the case when
       %% two jump want to happen at the same time. Ignore this issue for now.
       avail = (t + dt <= tstart || t >= tstart + tlen);
     end
+
     function [tstart, tlen] = timeSpan(self, len)
       tstart = 0;
       tlen = len;
     end
+
     function times = dirtyTime(self, ~)
       times = [];
     end
