a3d608d53b008930e57fc37ae166ddbb9520e96f
Yichao Yu <yyc1992@gmail.com>
2014-11-10 07:41:36 -0500

    [matlab_new/NiDACBackend] add empty class

---
+++ NiDACBackend.m
@@ -0,0 +1,45 @@
+%% Copyright (c) 2014-2014, Yichao Yu <yyc1992@gmail.com>
+%%
+%% This library is free software; you can redistribute it and/or
+%% modify it under the terms of the GNU Lesser General Public
+%% License as published by the Free Software Foundation; either
+%% version 3.0 of the License, or (at your option) any later version.
+%% This library is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+%% Lesser General Public License for more details.
+%% You should have received a copy of the GNU Lesser General Public
+%% License along with this library.
+
+classdef NiDACBackend < PulseBackend
+  properties(Hidden, Access=private)
+  end
+
+  methods
+    function self = NiDACBackend(varargin)
+      self = self@PulseBackend(varargin{:});
+      %% TODO
+    end
+
+    function val = getPriority(self)
+      val = 1;
+    end
+
+    function initDev(self, did)
+      %% TODO
+    end
+
+    function initChannel(self, did, cid)
+      %% TODO
+    end
+
+    function generate(self, seq, cids)
+      %% TODO
+      %% use clock_div
+    end
+
+    function run(self, rep)
+      %% TODO
+    end
+  end
+end
