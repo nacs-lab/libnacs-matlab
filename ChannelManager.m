50d1c6ff312eec3c384c507df748c60d762ed9b7
Yichao Yu <yyc1992@gmail.com>
2014-11-12 15:49:47 -0500

    [matlab_new/ChannelManager] add channel manager to use a integer for representing channels in order to work around MATLAB poor performance

---
+++ ChannelManager.m
@@ -0,0 +1,37 @@
+%% Copyright (c) 2014-2014, Yichao Yu <yyc1992@gmail.com>
+%%
+%% This library is free software; you can redistribute it and/or
+%% modify it under the terms of the GNU Lesser General Public
+%% License as published by the Free Software Foundation; either
+%% version 3.0 of the License, or (at your option) any later version.
+%% This library is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+%% Lesser General Public License for more details.
+%% You should have received a copy of the GNU Lesser General Public
+%% License along with this library.
+
+classdef ChannelManager < handle
+  properties
+    channels = [];
+  end
+  properties(Access=private)
+    cid_map;
+  end
+
+  methods
+    function self = ChannelManager()
+      cid_map = containers.Map();
+    end
+
+    function id = getId(self, name)
+      try
+        id = self.cid_map(name);
+      catch
+        id = size(self.channels, 1) + 1;
+        self.channels(id) = name;
+        self.cid_map(name) = id;
+      end
+    end
+  end
+end
