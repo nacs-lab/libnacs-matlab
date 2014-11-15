5ca7277f9f67d526c6b28ae715aa608e455341b2
Yichao Yu <yyc1992@gmail.com>
2014-11-15 18:10:48 -0500

    [matlab_new/ExpSeq] WIP: loader

--- ExpSeq.m
+++ ExpSeq.m
@@ -13,7 +13,10 @@
 
 classdef ExpSeq < ExpSeqBase
   properties(Access=private)
+    driver_names;
     drivers;
+
+    name_map;
   end
 
   methods
@@ -25,6 +28,51 @@ classdef ExpSeq < ExpSeqBase
       end
       self = self@ExpSeqBase(name);
       self.drivers = containers.Map();
+      self.name_map = containers.Map();
+    end
+
+    function cid = translateChannel(self, name)
+      name = self.transChnName(name);
+      cpath = strsplit(name, '/');
+      did = cpath{1};
+      driver = self.loadDriver(did);
+      cid = translateChannel@ExpSeqBase(self, name);
+      driver.initChannel(cid);
+    end
+
+    function cid = findChannelId(self, name)
+      name = self.transChnName(name);
+      cid = findChannelId@ExpSeqBase(self, name);
     end
   end
+
+  methods(Access=private)
+    function driver = loadDriver(self, did)
+      %% TODO
+    end
+
+    function res = transChnName(self, name)
+      try
+        res = self.name_map(name);
+      catch
+        cpath = strsplit(name, '/');
+        self.name_map(name) = [];
+        if self.channelAlias.isKey(cpath{1})
+          cpath{1} = self.channelAlias(cpath{1});
+          res = self.transChnName(strjoin(cpath, '/'));
+        else
+          res = name;
+        end
+        self.name_map(name) = res;
+        return;
+      end
+      if isempty(res)
+        error('Alias loop detected: %s.', name);
+      end
+    end
+  end
+
+  %% TODO translateChannel
+  %% getDefault
+  %% findDriver
 end
