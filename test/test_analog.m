fbae26218e6618b40946d512b560e3046672055f
Yichao Yu <yyc1992@gmail.com>
2014-11-18 21:24:08 -0500

    [matlab_new/test]

---
+++ test/test_analog.m
@@ -0,0 +1,27 @@
+%% Copyright (c) 2014-2014, Yichao Yu <yyc1992@gmail.com>
+%%
+%% This library is free software; you can redistribute it and/or
+%% modify it under the terms of the GNU Lesser General Public
+%% License as published by the Free Software Foundation; either
+%% version 3.0 of the License, or (at your option) any later version.
+%% This library is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+%% Lesser General Public License for more details.
+%% You should have received a copy of the GNU Lesser General Public
+%% License along with this library.
+
+s = daq.createSession('ni');
+s.Rate = 5e5;
+s.addAnalogOutputChannel('Dev2', 0, 'Voltage');
+s.addTriggerConnection('External', 'Dev2/PFI1', 'StartTrigger');
+s.addClockConnection('External', 'Dev2/PFI0', 'ScanClock');
+s.addAnalogOutputChannel('Dev2', 1, 'Voltage');
+s.addAnalogOutputChannel('Dev2', 2, 'Voltage');
+s.addAnalogOutputChannel('Dev2', 3, 'Voltage');
+s.queueOutputData(zeros(6, 4));
+s.startBackground();
+disp('Start Pause');
+pause(10);
+disp('Finish Pause');
+s.wait();
