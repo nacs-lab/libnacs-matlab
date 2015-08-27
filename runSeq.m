1588d4e94508c396552ad770302fcb8ff5d94541
nigrp <niharvardlab@gmail.com>
2015-08-26 21:21:09 -0400

    Daily update.  Nick

--- runSeq.m
+++ runSeq.m
@@ -202,7 +202,6 @@ else
             % If another instance has asked runSeq to abort, exit gracefully
             if m.Data(1).AbortRunSeq == 1
                 disp('AbortRunSeq set to 1.  Stopping gracefully.')
-                whos
                 %                 m.Data(1).AbortRunSeq = 0;
                 break
             end
@@ -228,7 +227,6 @@ if return_array
     params = params_array;
 end
 disp(['Finished running ' int2str(m.Data(1).CurrentSeqNum) ' sequences.'])
-whos
 m.Data(1).CurrentSeqNum = 0;
 m.Data(1).AbortRunSeq = 0;
 m.Data(1).PauseRunSeq = 0;
