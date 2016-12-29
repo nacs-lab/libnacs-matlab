61f72d36b65dc66639bac2e2a352a2a42ce60f2a
Tweezer 2 <niharvardlab@gmail.com>
2016-12-29 15:28:30 -0500

    Reset Ni DAQ session cache

--- resetGlobal.m
+++ resetGlobal.m
@@ -1,6 +1,11 @@
 function resetGlobal()
   global nacsTimeSeqDisableRunHack;
   global nacsTimeSeqNameSuffixHack;
+  global nacsNiDACBackendSession;
   nacsTimeSeqDisableRunHack = 0;
   nacsTimeSeqNameSuffixHack = [];
+  if isempty(nacsNiDACBackendSession)
+    delete(nacsNiDACBackendSession);
+    nacsNiDACBackendSession = [];
+  end
 end
