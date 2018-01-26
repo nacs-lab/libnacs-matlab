b64073a2e55442e2d25bead1dc5424c1414ad527
Yichao Yu <yyc1992@gmail.com>
2018-01-25 23:35:54 -0500

    [lib] FPGA poster

---
+++ FPGAPoster.m
@@ -0,0 +1,77 @@
+%% Copyright (c) 2017-2017, Yichao Yu <yyc1992@gmail.com>
+%%
+%% This library is free software; you can redistribute it and/or
+%% modify it under the terms of the GNU Lesser General Public
+%% License as published by the Free Software Foundation; either
+%% version 3.0 of the License, or (at your option) any later version.
+%% This library is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+%% Lesser General Public License for more details.
+%% You should have received a copy of the GNU Lesser General Public
+%% License along with this library.
+
+classdef FPGAPoster < handle
+  properties
+    poster;
+  end
+
+  methods(Access = private)
+    function self = FPGAPoster(url)
+      [path, ~, ~] = fileparts(mfilename('fullpath'));
+      pyglob = py.dict(pyargs('mat_srcpath', path, 'url', url));
+      try
+        py.exec('from FPGAPoster import FPGAPoster', pyglob);
+      catch
+        py.exec('import sys; sys.path.append(mat_srcpath)', pyglob);
+        py.exec('from FPGAPoster import FPGAPoster', pyglob);
+      end
+      self.poster = py.eval('FPGAPoster(url)', pyglob);
+    end
+  end
+
+  methods
+    function post(self, data)
+      try
+        self.poster.post(data);
+      catch ex
+        self.poster.recreate_sock();
+        rethrow(ex);
+      end
+    end
+
+    function wait(self, id)
+      try
+        while ~self.poster.post_reply()
+        end
+      catch ex
+        self.poster.recreate_sock();
+        rethrow(ex);
+      end
+    end
+
+    function msg=prepare_msg(self, tlen, code)
+      msg = self.poster.prepare_msg(tlen, code);
+    end
+  end
+
+  methods(Static)
+    function dropAll()
+      global nacsFPGAPosterCache
+      nacsFPGAPosterCache = [];
+    end
+    function res = get(url)
+      global nacsFPGAPosterCache
+      if isempty(nacsFPGAPosterCache)
+        nacsFPGAPosterCache = containers.Map();
+      end
+      cache = nacsFPGAPosterCache;
+      if isKey(cache, url)
+        res = cache(url);
+        return;
+      end
+      res = FPGAPoster(url);
+      cache(url) = res;
+    end
+  end
+end
---
+++ FPGAPoster.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2018-2018, Yichao Yu <yyc1992@gmail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3.0 of the License, or (at your option) any later version.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.
+
+import zmq
+import struct
+import sys
+import libnacs
+
+class FPGAPoster(object):
+    def recreate_sock(self):
+        self.__sock = self.__ctx.socket(zmq.REQ)
+        self.__sock.connect(self.__url)
+
+    def __init__(self, url):
+        self.__url = url
+        self.__ctx = zmq.Context()
+        self.recreate_sock()
+
+    def __del__(self):
+        self.__ctx.destroy()
+
+    def prepare_msg(self, tlen, code):
+        bc = int(tlen).to_bytes(8, byteorder='little', signed=False)
+        bc += libnacs.bin_to_bytecode(code)
+        return bc
+
+    def post(self, data):
+        self.__sock.send_string("run_seq", zmq.SNDMORE)
+        self.__sock.send(b'\0\0\0\0', zmq.SNDMORE) # version
+        self.__sock.send(data)
+
+    def post_reply(self):
+        if self.__sock.poll(1000) == 0:
+            return False
+        self.__sock.recv()
+        return True
