f6f781d908da985fee51031f81998c851d5113b3
Tweezer 2 <niharvardlab@gmail.com>
2018-06-27 13:56:37 -0400

    Add a sequence to test turning ttl channels on and off with variable lengths

---
+++ test/test_ttl_timing.m
@@ -0,0 +1,12 @@
+function test_ttl_timing(chn, init, times, mgrarg)
+s = ExpSeq();
+if exist('mgrarg', 'var')
+    s.addOutputMgr(mgrarg{:});
+end
+v = init;
+for t = times
+    s.addStep(t).add(chn, v);
+    v = ~v;
+end
+s.run();
+end
