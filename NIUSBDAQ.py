2e40f03ff7e3b59fc362d9795de439e17d1d6d7c
Lewis <lewispicard@gmail.com>
2019-12-18 16:02:43 -0500

    B Field compensation code and instrument interfaces

---
+++ NIUSBDAQ.py
@@ -0,0 +1,72 @@
+import nidaqmx
+from nidaqmx.constants import AcquisitionType, TaskMode, Signal, Edge
+from nidaqmx import stream_writers as writers
+from nidaqmx import stream_readers as readers
+import numpy as np
+import time
+import sys
+
+
+def initDAQ(devNum = 0):
+    #Check NI DAQ device is connected and return device handle
+    #Arguments: 
+    #    devNum: the number of the NI DAQ device to connect to. If only connected 
+    #        device, devNum = 0, otherwise number in order of connections
+    
+    system = nidaqmx.system.System.local()
+    devs = system.devices
+    try:
+        daq = system.devices[devNum]
+        return daq
+    except IndexError as err:
+        print("No DAQ number ",devNum)
+        raise 0
+    except Exception as err:
+         raise err
+
+def acquire(devNum,channelName, sampleRate,sampleTime, bTrig = 0, trigChan = "PFI0"):
+    #Record voltage input on channel ai0 for a fixed time
+    #Arguments:
+    #    devNum: the number of the NI DAQ device to connect to.
+    #   channelName: The name of the channel to read from, e.g. "ai0"
+    #   sampleTime: total time for which to acquire signal (in s)
+    #   sampleRate: NI DAQ sample rate (in Hz)
+    #   bTrig: bool, 1 to use trigger on PFI0, 0 otherwise
+    
+    
+    #Check NI DAQ connected and reset
+    try:
+         daq = initDAQ(devNum)
+    except IndexError:
+         print("No NI DAQ device connected")
+         raise SystemExit
+    except Exception:
+         print("Unknown Error")
+         raise SystemExit
+    with nidaqmx.Task() as task:
+        channelAddr = "Dev%d/%s" % (devNum+1,channelName)
+        task.ai_channels.add_ai_voltage_chan(channelAddr) 
+        
+        #Record a fixed number of samples on ai0 then return
+        nSamples = int(sampleTime*sampleRate)
+        task.timing.cfg_samp_clk_timing(sampleRate, sample_mode=AcquisitionType.FINITE, samps_per_chan=nSamples)
+        if bTrig:
+            task.triggers.start_trigger.cfg_dig_edge_start_trig(trigChan, Edge.RISING)
+        
+        task.start()
+        samples = task.read(number_of_samples_per_channel=nSamples, timeout = 1.5*sampleTime)
+
+    return samples
+    
+def getSerial(devNum):
+    #Return the serial number for device number devNum
+    daq = initDAQ(devNum)
+    if daq == 0:
+        return 0
+    return daq.dev_serial_num
+
+def numDevices():
+    system = nidaqmx.system.System.local()
+    devs = system.devices
+    return len(devs)
+    
---
+++ NIUSBDAQWrapper.m
@@ -0,0 +1,165 @@
+classdef NIUSBDAQWrapper < handle
+    %NIUSBDAQPoster Python wrapper for control of NI USB DAQ
+    %Requires nidaqmx python library installed
+    
+    properties(Constant, Access=private)
+        cache = containers.Map();
+    end
+    
+    properties(SetAccess = private)
+        serialNum;
+        pyglob;
+    end
+    
+    properties
+        devNum;
+        channelName = "ai0";
+        trigChan = "PFI0";
+        bTrig = 1;
+    end
+    
+    methods
+        function res = setChannels(self,channelSettings)
+            %Set data acquisition and (optionally) triggering properties
+            %ChannelSettings, struct with fields:
+            %   channelName, string: Name of the channel to read from, e.g. "ai0"
+            %   bTrig, bool: 1 to do triggered acquisition, 0 to autostart
+            %   trigChan, string: Name of  channel to trigger from
+            
+            if isfield(channelSettings, 'channelName')
+                self.channelName = channelSettings.channelName;
+            end
+            if isfield(channelSettings, 'bTrig')
+                self.bTrig = channelSettings.bTrig;
+            end
+            if isfield(channelSettings, 'trigChan')
+                self.trigChan = channelSettings.trigChan;
+            end
+            res = self;
+        end
+        
+        function aiData = acquire(self, sampleRate, sampleTime, varargin)
+            %Acquire data from NI USB DAQ
+            %Arguments:
+            %   sampleRate, int: Number of samples per second to acquire
+            %   sampleTime, float: Number of seconds for which to sample
+            %   optional argument:
+            %       channelSettings, struct: channelName, bTrig, trigChan
+            
+            %Set optional arguments to overwrite current values
+            if ~isempty(varargin)
+                channelSettings = varargin{1};
+                if isa(channelSettings, 'struct')
+                    self = self.setChannels(channelSettings);
+                else
+                    warning('channelSettings input must be a struct')
+                end
+            end
+
+            %Acquire data and convert to numeric array
+            aiData = cell2mat(cell(py.NIUSBDAQ.acquire(self.devNum,self.channelName,...
+                sampleRate, sampleTime,self.bTrig, self.trigChan)));
+        end
+    end
+    
+    methods(Access = private)
+        function self = NIUSBDAQWrapper(devNum,serialNum)
+            %NIUSBDAQWrapper Construct an instance of this class
+            self.devNum = devNum;
+            self.serialNum = serialNum;
+        end
+    end
+    
+    methods(Static)
+        function dropAll()%Delete NIUSBDAQWrapper connection from memory
+            remove(NIUSBDAQWrapper.cache, keys(NIUSBDAQWrapper.cache));
+        end
+        
+        function res = get(serialNum, varargin)
+            %Create new object or return existing object
+            %Arguments:
+            %   serialNum, int: Serial number of USB DAQ to connect to
+            %   ChannelSettings, struct with fields:
+            %       channelName, string: Name of the channel to read from, e.g. "ai0"
+            %       bTrig, bool: 1 to do triggered acquisition, 0 to autostart
+            %       trigChan, string: Name of  channel to trigger from
+            
+            cache = NIUSBDAQWrapper.cache;
+            
+            %Get path of class definition, which should be in same place as
+            %Python library
+            [path, ~, ~] = fileparts(mfilename('fullpath'));
+            pyglob = py.dict(pyargs('mat_srcpath', path,'serialNum',serialNum));
+            
+            %Load python library
+            try
+                py.exec('import NIUSBDAQ', pyglob);
+            catch
+                py.exec('import sys; sys.path.append(mat_srcpath)', pyglob);
+                py.exec('import NIUSBDAQ', pyglob);
+            end
+            
+            %Get number of devices connected
+            numDevs = int64(py.NIUSBDAQ.numDevices());
+            
+            if numDevs == 0
+                disp('Error: No NI USB DAQ devices connected.')
+                res = [];
+                return
+            end
+            
+            %Search for device with given serial number
+            devNum = -1;
+            for i = 0:(numDevs - 1)
+                checkSerial = int64(py.NIUSBDAQ.getSerial(i));
+                if checkSerial == serialNum
+                    devNum = i;
+                    break
+                end
+            end
+            
+            if devNum == -1
+                warning('No device found with given serial number. B field comp is not working.')
+                res = [];
+                return
+            end
+            
+            %Generate id from serial number
+            id = NIUSBDAQWrapper.getID(serialNum);
+            
+            %Check if connection to instrument already exists
+            if isKey(cache, id) 
+                res = cache(id);
+                if ~isempty(res)
+                    res.devNum = devNum; %Reset device number to located device
+                    return;
+                end
+                delete(res);
+            end
+            
+            %If no connection exists, initialize
+            res = NIUSBDAQWrapper(devNum, serialNum);
+            cache(id) = res;
+            
+            %Set optional arguments to overwrite current values
+            if ~isempty(varargin)
+                channelSettings = varargin{1};
+                if isa(channelSettings, 'struct')
+                    res = res.setChannels(channelSettings);
+                else
+                    warning('channelSettings input must be a struct')
+                end
+            end
+            
+            %Updated global python environment
+            res.pyglob = py.dict(pyargs('mat_srcpath', path,'serialNum',serialNum,...
+                'trigChan', res.trigChan,'channelName',res.channelName, 'bTrig', res.bTrig));
+        end
+        
+        function id = getID(serialNum)
+            %Generate unique device id from serial number
+            id = sprintf('NI-USB-DAQ:serial#%d', serialNum);
+        end
+    end
+end
+
