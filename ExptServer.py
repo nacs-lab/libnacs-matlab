be11a2b246ccf0358b4e2c3e57bd04471c39210d
kwang428 <kenneth.wang15@gmail.com>
2021-09-22 10:26:10 -0400

    get rid of transferer in ExptServer among others

--- ExptServer.py
+++ ExptServer.py
@@ -3,7 +3,6 @@ from enum import Enum
 import threading
 from collections import deque
 import array
-import asyncio
 import time
 
 class ExptServer(object):
@@ -18,154 +17,6 @@ class ExptServer(object):
         NoRequest = 0
         Pause = 1
         Abort = 2
-    class Transferer(object):
-        class Request(object):
-            class ReqType(Enum):
-                Stop = 0
-                SaveImgs = 1
-                SeqEnd = 2
-                SetConfig = 3
-            def __init__(self, req_type, *args):
-                self.type = req_type
-                self.args = args
-            def __bool__(self):
-                return self.type == self.ReqType.Stop
-
-        def __init__(self, parent):
-            self.parent = parent
-            self.__req_lock = threading.Lock()
-            self.__data_lock = threading.Lock()
-            self.__worker = threading.Thread(target = self.__worker_func)
-            with self.__req_lock:
-                self.reqs = deque()
-            with self.__data_lock:
-                self.imgs = deque()
-                self.nseq = 0
-                self.dateStamp = ""
-                self.timeStamp = ""
-                self.nseq_imgs = 0 # number of sequences of images stored
-            self.__worker.start()
-            self.temp_imgs = []
-
-        def start(self):
-            if hasattr(self, '__worker'):
-                if self.__worker.is_active():
-                    return
-            self.__worker = threading.Thread(target = self.__worker_func)
-            self.__worker.start()
-
-        def add_req(self, req_type, *args):
-            with self.__req_lock:
-                self.reqs.appendleft(self.Request(req_type, *args))
-
-        def stop(self):
-            if hasattr(self, '__worker'):
-                self.add_req(self.Request.ReqType.Stop)
-                self.__worker.join()
-            else:
-                return
-
-        def reset(self):
-            self.stop()
-            with self.__req_lock:
-                self.reqs = deque()
-            with self.__data_lock:
-                self.imgs = deque()
-                self.nseq = 0
-                self.dateStamp = ""
-                self.timeStamp = ""
-                self.nseq_imgs = 0 # number of sequences of images stored
-            self.temp_imgs = []
-            self.start()
-
-        def send_img_req(self, data):
-            self.add_req(self.Request.ReqType.SaveImgs, data)
-
-        def send_end_seq(self):
-            self.add_req(self.Request.ReqType.SeqEnd)
-
-        def send_set_config(self, date, time):
-            self.add_req(self, Request.ReqType.SetConfig, date, time)
-
-        def __check_worker_req(self) -> bool:
-        # return True if need to stop
-            with self.__req_lock:
-                try:
-                    res = self.reqs.pop()
-                except:
-                    res = None
-            return res
-
-        def pop_img(self):
-            with self.__data_lock:
-                try:
-                    res = self.imgs.pop()
-                except:
-                    res = None
-            return res
-
-        def handle_req(self, req):
-            if req.type == self.Request.ReqType.SaveImgs:
-                self.temp_imgs.append(req.args[0])
-            elif req.type == self.Request.ReqType.SeqEnd:
-                with self.__data_lock:
-                    self.nseq = self.nseq + 1
-                    self.nseq_imgs = self.nseq_imgs + 1
-                    for data in self.temp_imgs:
-                        self.imgs.appendleft(data)
-                    self.imgs.appendleft(b'')
-                self.temp_imgs.clear()
-            elif req_type == self.Request.ReqType.SetConfig:
-                with self.__data_lock:
-                    self.dateStamp = req.args[0]
-                    self.timeStamp = req.args[1]
-            else:
-                print(f"Unknown request: {req.type}")
-            self.__this_worker_req = None
-
-        def __worker_func(self):
-            req = self.__check_worker_req()
-            while not req:
-                if req is not None:
-                    self.handle_req(req)
-                time.sleep(0.2)
-                req = self.__check_worker_req()
-        # for other threads to extract information
-        def get_seq_num(self) -> int:
-            with self.__data_lock:
-                return self.nseq
-        def get_num_imgs(self) -> int:
-            with self.__data_lock:
-                return self.nseq_imgs
-        def get_config(self):
-            with self.__data_lock:
-                return self.dateStamp, self.timeStamp
-
-        def get_imgs(self, nseqs=-1):
-            # nseqs = -1 gets all images currently stored
-            # otherwise, get number of seqs specified by nseq.
-            # this function will block, so other network requests may not be handled. Suggested usage, use nseqs=-1, or first get_num_imgs and only request that many
-            # returns bytes to be sent across the network
-            # intended format: [[shape_x: 4B, shape_y: 4B, nimgs: 4B, data: shape_x * shape_y * nimgs * bytes_per_pixel] x num_transfers_per_seq] [<0>:4B] x nseqs]
-            # 0 separates out sequences
-            n_transfer = 0
-            res = bytearray()
-            if nseqs < 0:
-                nseqs = self.get_num_imgs()
-            while n_transfer < nseqs:
-                next_img = self.pop_img()
-                while next_img != b'':
-                    if next_img is None:
-                        # more imgs are requested than actually are saved...
-                        break
-                    else:
-                        res.extend(next_img.tobytes())
-                    next_img = self.pop_img()
-                res.extend(int.to_bytes(0, 4, 'little'))
-                n_transfer = n_transfer + 1
-            with self.__data_lock:
-                self.nseq_imgs = self.nseq_imgs - n_transfer
-            return res
 
     def recreate_sock(self):
         if self.__sock is not None:
@@ -196,8 +47,7 @@ class ExptServer(object):
         self.__worker = threading.Thread(target = self.__worker_func)
         self.__worker.start()
 
-        # transferer
-        #self.transferer = self.Transferer()
+        # data variables
         with self.__expt_lock:
             self.expt_imgs = deque() # this deque is the one the expt thread uses.
         with self.__data_lock:
@@ -207,6 +57,7 @@ class ExptServer(object):
             self.timeStamp = ""
             self.nseq_imgs = 0 # number of sequences of images stored
         self.temp_imgs = [] # stored mid sequence
+
         # status of seq
         with self.__seq_lock:
             self.__seq_req = self.SeqRequest.NoRequest
@@ -214,13 +65,11 @@ class ExptServer(object):
             self.seq_status = self.State.Init
 
     def __del__(self):
-        #self.transferer.stop()
         self.stop_worker()
         self.__sock.close()
         self.__ctx.destroy()
 
     def reset(self):
-        #self.transferer.reset()
         self.stop_worker()
         self.recreate_sock()
         with self.__expt_lock:
@@ -268,13 +117,7 @@ class ExptServer(object):
         elif msg_str == "get_status":
             rep = self.get_status()
             self.safe_send_string(addr, rep)
-            pass
         elif msg_str == "get_imgs":
-            #msg = self.safe_recv()
-            #if msg is None:
-            #    rep = self.transferer.get_imgs()
-            #else:
-            #    rep = self.transferer.get_imgs(int.from_bytes(msg, 'little'))
             rep = self.get_imgs()
             self.safe_send(addr, rep)
         elif msg_str == "get_seq_num":
@@ -286,9 +129,8 @@ class ExptServer(object):
         elif msg_str == "get_config":
             datestr, timestr = self.get_config()
             self.safe_send_string(addr, datestr, zmq.SNDMORE)
-            self.send_string(time_str)
+            self.__sock.send_string(timestr)
         else:
-            #print(f"unknown {msg_str}")
             self.safe_send_string(addr, f'')
             return False
         return True
@@ -340,7 +182,7 @@ class ExptServer(object):
 
     def __worker_func(self):
         # worker function
-        while self.__check_worker_req != self.WorkerRequest.Stop:
+        while self.__check_worker_req() != self.WorkerRequest.Stop:
             if self.__sock.poll(self.timeout) == 0: # in milliseconds
                 continue
             addr = self.safe_recv()
@@ -365,7 +207,7 @@ class ExptServer(object):
 
     def abort_seq(self) -> str:
         with self.__data_lock:
-            if self.seq_status == self.State.Running:
+            if self.seq_status == self.State.Running or self.seq_status == self.State.Paused:
                 self.seq_status = self.State.Init
                 with self.__seq_lock:
                     self.__seq_req = self.SeqRequest.Abort
@@ -377,7 +219,7 @@ class ExptServer(object):
     def get_status(self) -> str:
         with self.__data_lock:
             if self.seq_status == self.State.Init:
-                res = "Sequence has not started"
+                res = "Sequence is stopped"
             elif self.seq_status == self.State.Paused:
                 res = "Sequence is paused"
             elif self.seq_status == self.State.Running:
@@ -408,25 +250,29 @@ class ExptServer(object):
 
     def get_imgs(self):
         # returns bytes to be sent across the network
-        # intended format: [[shape_x: 4B, shape_y: 4B, nimgs: 4B, data: shape_x * shape_y * nimgs * bytes_per_pixel] x num_transfers_per_seq] [<0>:4B] x nseqs]
+        # intended format: [nseqs: double][[scan_id: double][shape_x: double, shape_y: double, nimgs: double, data: shape_x * shape_y * nimgs * sizeof(double)] x num_transfers_per_seq] [<0>:double] x nseqs]
         # 0 separates out sequences
-        #n_transfer = 0
+        n_transfer = 0
+        zero_array = array.array('d', [0])
         res = bytearray()
         #if nseqs < 0:
         nseqs = self.get_num_imgs()
+        nseqs_array = array.array('d', [nseqs])
+        res.extend(nseqs_array.tobytes())
         #swap out. Assume self.imgs has been cleared out already
-        with self.__data_lock:
-            with self.__expt_lock:
-                self.expt_imgs, self.imgs = self.imgs, self.expt_imgs
-        while n_transfer < nseqs:
-            next_img = self.pop_img()
-            while next_img != b'':
-                res.extend(next_img.tobytes())
+        if nseqs > 0:
+            with self.__data_lock:
+                with self.__expt_lock:
+                    self.expt_imgs, self.imgs = self.imgs, self.expt_imgs
+            while n_transfer < nseqs:
                 next_img = self.pop_img()
-            res.extend(int.to_bytes(0, 4, 'little'))
-            n_transfer = n_transfer + 1
-        with self.__data_lock:
-            self.nseq_imgs = self.nseq_imgs - n_transfer
+                while next_img != b'':
+                    res.extend(next_img.tobytes())
+                    next_img = self.pop_img()
+                res.extend(zero_array.tobytes())
+                n_transfer = n_transfer + 1
+            with self.__data_lock:
+                self.nseq_imgs = self.nseq_imgs - n_transfer
         return res
 
     # this one is only for msg handler
@@ -435,6 +281,7 @@ class ExptServer(object):
             if self.seq_status == self.State.Paused:
                 with self.__seq_lock:
                     self.__seq_req = self.SeqRequest.NoRequest
+                self.seq_status = self.State.Running
                 res = "Sequence should now be running"
             else:
                 res = "Sequence was not in Paused state. To start a new sequence, use the main MATLAB instance"
@@ -443,27 +290,33 @@ class ExptServer(object):
     # functions for the Expt thread
     def check_request(self):
         with self.__seq_lock:
-            res = self.__seq_req
+            res = self.__seq_req.value # get value for Matlab
         return res
 
     def start_seq(self):
         with self.__data_lock:
-            self.seq_status == self.State.Running
+            self.seq_status = self.State.Running
+        #clear abort or pause if exists
+        with self.__seq_lock:
+            self.__seq_req = self.SeqRequest.NoRequest
+        scan_id = round(time.time() * 1000) # assuming scans aren't started within ms of each other... We'll send over as 64 bits over the network
+        return scan_id
 
-    def store_imgs(self, data):
+    def store_imgs(self, data, scan_id=-1, seq_id=-1):
         # need to make data an array, so tobytes can be called
-        #self.transferer.send_img_req(data)
+        if not self.temp_imgs: # check it temp_imgs is empty
+            self.temp_imgs.append(array.array('d', [scan_id]))
+            self.temp_imgs.append(array.array('d', [seq_id]))
         self.temp_imgs.append(data)
 
     def seq_finish(self):
-        #self.transferer.send_end_seq()
         with self.__data_lock:
             self.nseq = self.nseq + 1
             self.nseq_imgs = self.nseq_imgs + 1
             with self.__expt_lock:
                 for data in self.temp_imgs:
-                    self.imgs.appendleft(data)
-                self.imgs.appendleft(b'')
+                    self.expt_imgs.appendleft(data)
+                self.expt_imgs.appendleft(b'')
         self.temp_imgs.clear()
 
     def set_config(self, date: str, time: str):
